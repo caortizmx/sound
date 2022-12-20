75e5e00f540317917a2eb4b5ed4c2f47e4cda52e
Carlos Ortiz <carlos.ortiz@bigcommerce.com>
2022-12-19 19:01:42 -0600

    swtich contact image and style

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import turntable from '../turntable.jpg'
+import React from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { Link } from 'react-router-dom';
+import contactM from '../contact-m.jpg';
+import contactW from '../contact-w.jpg';
 
 function Contact() {
+  const isMobile = useMediaQuery({ query: '(max-width: 600px)' });
   return (
     <div className='contact'>
       <div className='email-container'>
-        <img id="contact-img" src={turntable} alt='turntable'></img>
+        <img id="contact-img" src={isMobile ? contactM : contactW} alt='turntable'/>
         <div className='link-container'>
           <Link onClick={() => window.location = 'mailto:contact@sounds-likeus.com'} id="email" target="_blank">
           contact@sounds-likeus.com
