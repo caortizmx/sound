93cd619509edb093a067a41bf65507cbad67a3fa
brun0z <bruno.u@protonmail.com>
2022-12-19 21:16:31 -0600

    Contact page change

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
-import { Link } from 'react-router-dom';
 import contactM from '../contact-m.jpg';
 import contactW from '../contact-w.jpg';
 
 function Contact() {
   const isMobile = useMediaQuery({ query: '(max-width: 600px)' });
   return (
-    <div className='contact'>
-      <div className='email-container'>
-        <img id="contact-img" src={isMobile ? contactM : contactW} alt='turntable'/>
-        <div className='link-container'>
-          <Link onClick={() => window.location = 'mailto:contact@sounds-likeus.com'} id="email" target="_blank">
-          contact@sounds-likeus.com
-          </Link>
-        </div>
-      </div>
+    <div className='contact' style={{ 
+    backgroundImage: `url(${isMobile ? contactM : contactW})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'}}>
+      <span onClick={() => window.location = 'mailto:contact@sounds-likeus.com'} id="email" target="_blank">
+      contact@sounds-likeus.com
+      </span>
     </div>
   )
 }
